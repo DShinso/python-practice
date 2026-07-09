03d059c443af2fc77087a881321c41de2a685738
Shinso Shiju <shinsod7@gmail.com>
2026-07-09 22:15:34 +0530

    Completed Day03 - Decision Making

---
+++ Day03/age_checker.py
@@ -0,0 +1,10 @@
+age = int(input("Enter your age: "))
+
+if age < 13:
+    print("You are a child! ")
+elif age <= 17:
+    print("You are a teenager! ")
+elif age < 60:
+    print("You are a adult! ")
+elif age >= 60:
+    print("You are Senior Citizen! ")
